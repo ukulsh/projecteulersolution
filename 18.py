0792e9a9dc6ebd4418a0172aa7bceb6b3d81b73c
utkarsh <kulshrestha.utkarsh@outlook.com>
2019-03-11 18:29:40 +0530

    Update 18.py

--- 18.py
+++ 18.py
@@ -1,9 +1,10 @@
 arr = []
-arr.append(list(map(int,input().split())))
-for i in range(14,0,-1):
+for arr_i in range(15):
+    arr_temp = map(int,input().strip().split(' '))
+    arr.append(list(arr_temp))
+print(arr,'\n')
+for i in range(13,-1,-1):
     for j in range(len(arr[i])):
-        if arr[i+1][j] > arr[i+1][j+1]:
-            arr[i][j] = arr[i+1][j]
-        else:
-            arr[i][j] = arr[i+1][j+1]
+        arr[i][j] += max(arr[i+1][j],arr[i+1][j+1])
 print(arr[0][0])
+
